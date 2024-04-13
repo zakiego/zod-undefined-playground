31db5776ee1f801b0e700800752710251a96fb95
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-04-13 15:14:57 +0800

    fix: grid in lg xl

--- src/app/with-zod/page.tsx
+++ src/app/with-zod/page.tsx
@@ -105,7 +105,7 @@ export default async function Home() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
           {data.products.map((product) => (
             <Card key={product.id}>
               <CardHeader>
--- src/app/without-zod/page.tsx
+++ src/app/without-zod/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 ">
           {data.products.map(
             (
               product, // strict mode set to false, so no error here
